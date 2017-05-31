1ca6aa818eb7c3ad0fcae4aa705a727d684823c2
Alberto D'Este <alberto.deste92@gmail.com>
2017-05-31 13:19:14 +0200

    Fixed duplicated table names bug

--- src/main/java/org/homer/neo4j/graphmanager/procedure/Init.java
+++ src/main/java/org/homer/neo4j/graphmanager/procedure/Init.java
@@ -27,62 +27,80 @@ public class Init {
     @Description("sql.manager.init(schema) - Create a Database node by importing from the database.")
     public Stream<NodeOutput> init(
             @Name("hostname") String hostname,
-			@Name("port") Long port,
-			@Name("database") String databaseName,
-			@Name("username") String username,
-			@Name("password") String password
+            @Name("port") Long port,
+            @Name("database") String databaseName,
+            @Name("username") String username,
+            @Name("password") String password
     ) throws SQLException {
         Node database = db.createNode();
-		database.addLabel(Label.label("Database"));
-		database.setProperty("name", databaseName);
+        database.addLabel(Label.label("Database"));
+        database.setProperty("name", databaseName);
 
 
         try (Connection con = DriverManager.getConnection(String.format("jdbc:postgresql://%s:%s/%s", hostname, port, databaseName), username, password)) {
 
-			try(ResultSet schemasRs = con.createStatement().executeQuery("SELECT DISTINCT table_schema FROM information_schema.tables WHERE table_schema NOT IN ('pg_catalog', 'information_schema')")){
-				while(schemasRs.next()){
-					Node schema = db.createNode();
-					schema.addLabel(Label.label("Schema"));
-					schema.setProperty("name", schemasRs.getString(1));
+            try (ResultSet schemasRs = con.createStatement().executeQuery("SELECT DISTINCT table_schema FROM information_schema.tables WHERE table_schema NOT IN ('pg_catalog', 'information_schema')")) {
+                while (schemasRs.next()) {
+                    Node schema = db.createNode();
+                    schema.addLabel(Label.label("Schema"));
+                    schema.setProperty("name", schemasRs.getString(1));
 
-					database.createRelationshipTo(schema, RelationshipType.withName("HAS_SCHEMA"));
+                    database.createRelationshipTo(schema, RelationshipType.withName("HAS_SCHEMA"));
 
-					try(ResultSet tablesRs = con.createStatement().executeQuery(String.format("SELECT table_name FROM information_schema.tables WHERE table_schema = '%s'", schema.getProperty("name")))) {
-						while (tablesRs.next()) {
-							Node table = db.createNode();
-							table.addLabel(Label.label("Table"));
-							table.setProperty("_name", tablesRs.getString(1));
+                    try (ResultSet tablesRs = con.createStatement().executeQuery(String.format("SELECT table_name FROM information_schema.tables WHERE table_schema = '%s'", schema.getProperty("name")))) {
+                        while (tablesRs.next()) {
+                            Node table = db.createNode();
+                            table.addLabel(Label.label("Table"));
+                            table.setProperty("_name", tablesRs.getString(1));
 
-							schema.createRelationshipTo(table, RelationshipType.withName("HAS_TABLE"));
+                            schema.createRelationshipTo(table, RelationshipType.withName("HAS_TABLE"));
 
-							try(ResultSet columnRs = con.createStatement().executeQuery(String.format("SELECT column_name, data_type, is_nullable FROM information_schema.columns WHERE table_schema = '%s' AND table_name = '%s'", schema.getProperty("name"), table.getProperty("_name")))){
-								while(columnRs.next()){
-									List<String> attributes = new LinkedList<>();
-									attributes.add(columnRs.getString(2));
-									if(columnRs.getBoolean(3)){
-										attributes.add("NOT NULL");
-									}
-									table.setProperty(columnRs.getString(1), attributes.toArray(new String[attributes.size()]));
-								}
-							}
+                            try (ResultSet columnRs = con.createStatement().executeQuery(String.format("SELECT column_name, data_type, is_nullable FROM information_schema.columns WHERE table_schema = '%s' AND table_name = '%s'", schema.getProperty("name"), table.getProperty("_name")))) {
+                                while (columnRs.next()) {
+                                    List<String> attributes = new LinkedList<>();
+                                    attributes.add(columnRs.getString(2));
+                                    if (columnRs.getBoolean(3)) {
+                                        attributes.add("NOT NULL");
+                                    }
+                                    table.setProperty(columnRs.getString(1), attributes.toArray(new String[attributes.size()]));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
 
-							try(ResultSet foreignKeysRs = con.createStatement().executeQuery(String.format("SELECT tc.constraint_name, tc.table_name, kcu.column_name, ccu.table_name AS foreign_table_name, ccu.column_name AS foreign_column_name FROM information_schema.table_constraints AS tc JOIN information_schema.key_column_usage AS kcu ON tc.constraint_name = kcu.constraint_name JOIN information_schema.constraint_column_usage AS ccu ON ccu.constraint_name = tc.constraint_name WHERE constraint_type = 'FOREIGN KEY' AND tc.table_name='%s' AND tc.table_schema='%s'", table.getProperty("_name"), schema.getProperty("name")))){
-								while(foreignKeysRs.next()){
-									Optional<Node> foreignTable = Optional.ofNullable(db.findNode(Label.label("Table"), "_name", foreignKeysRs.getString(4)));
-									if(foreignTable.isPresent()) {
-										Relationship relationship = table.createRelationshipTo(foreignTable.get(), RelationshipType.withName("RELATION"));
-										relationship.setProperty("constraint", foreignKeysRs.getString(1));
-										relationship.setProperty("source_column", foreignKeysRs.getString(3));
-										relationship.setProperty("destination_column", foreignKeysRs.getString(5));
-									}
-								}
-							}
-						}
-					}
-				}
-			}
+            try (ResultSet foreignKeysRs = con.createStatement().executeQuery("SELECT DISTINCT tc.constraint_name, tc.table_name, tc.constraint_schema, kcu.column_name, ccu.table_name AS foreign_table_name, ccu.table_schema, ccu.column_name AS foreign_column_name FROM information_schema.table_constraints AS tc JOIN information_schema.key_column_usage AS kcu ON tc.constraint_name = kcu.constraint_name JOIN information_schema.constraint_column_usage AS ccu ON ccu.constraint_name = tc.constraint_name WHERE constraint_type = 'FOREIGN KEY'")) {
+                while (foreignKeysRs.next()) {
+                    String constraintName = foreignKeysRs.getString(1);
+                    String sourceTableName = foreignKeysRs.getString(2);
+                    String sourceSchemaName = foreignKeysRs.getString(3);
+                    String sourceColumnName = foreignKeysRs.getString(4);
+                    String destinationTableName = foreignKeysRs.getString(5);
+                    String destinationSchemaName = foreignKeysRs.getString(6);
+                    String destinationColumnName = foreignKeysRs.getString(7);
+
+                    Optional<Node> sourceTable = Optional.ofNullable(db.findNodes(Label.label("Schema"), "name", sourceSchemaName).stream()
+                            .map(n -> getChildTableWithName(n, sourceTableName)).findFirst().orElse(null));
+
+                    Optional<Node> destinationTable = Optional.ofNullable(db.findNodes(Label.label("Schema"), "name", destinationSchemaName).stream()
+                            .map(n -> getChildTableWithName(n, destinationTableName)).findFirst().orElse(null));
+
+                    if (sourceTable.isPresent() && destinationTable.isPresent()) {
+                        Relationship relationship = sourceTable.get().createRelationshipTo(destinationTable.get(), RelationshipType.withName("RELATION"));
+                        relationship.setProperty("constraint", constraintName);
+                        relationship.setProperty("source_column", sourceColumnName);
+                        relationship.setProperty("destination_column", destinationColumnName);
+                    }
+                }
+            }
         }
 
         return Stream.of(new NodeOutput(database));
+    }
+
+    Node getChildTableWithName(Node node, String tableName) {
+        return StreamSupport.stream(node.getRelationships(RelationshipType.withName("HAS_TABLE")).spliterator(), false)
+                .filter(r -> tableName.equals(r.getEndNode().getProperty("_name"))).map(Relationship::getEndNode).findFirst().orElse(null);
     }
 }
